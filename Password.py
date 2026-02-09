e021ff172ca04f97b26d3f2c94a8d6bfc5e1f9a2
Rohit Parit <rohitparit70@gmail.com>
2026-02-09 21:37:55 +0530

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,22 @@
+import re
+
+def check_password_strength(password):
+    if len(password) < 8:
+        return "Weak - Minimum 8 characters required"
+
+    if not re.search("[A-Z]", password):
+        return "Weak - Add uppercase letter"
+
+    if not re.search("[a-z]", password):
+        return "Weak - Add lowercase letter"
+
+    if not re.search("[0-9]", password):
+        return "Weak - Add number"
+
+    if not re.search("[@#$%^&*!]", password):
+        return "Weak - Add special character"
+
+    return "Strong Password"
+
+pwd = input("Enter password: ")
+print(check_password_strength(pwd))
